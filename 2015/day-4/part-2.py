eaf873b458a74742ee14088e78d8b9dd6a68505a
Andres <andres99.dev@gmail.com>
2024-12-11 22:30:01 +0100

    2015 day 4

---
+++ 2015/day-4/part-2.py
@@ -0,0 +1,15 @@
+import os
+import hashlib
+
+file_path = os.path.join(os.path.dirname(__file__), "./input.txt")
+# file_path = os.path.join(os.path.dirname(__file__), "./test-input.txt")
+input = open(file_path).read().strip()
+
+
+number = 0
+while True:
+    result = hashlib.md5((input + str(number)).encode()).hexdigest()
+    if result.startswith("000000"):
+        print(number)
+        break
+    number += 1
